61efe87eec321176bc412c433c33ab183d8cf707
ivan Vitti <ivan.vitti0@gmail.com>
2023-10-12 00:21:03 -0300

    Exer05

--- Exercicio05/src/Exe05/Atributos.java
+++ Exercicio05/src/Exe05/Atributos.java
@@ -2,4 +2,46 @@ package Exe05;
 
 public class Atributos {
 
+	private int numero_conta;
+	private String nome;
+	private double deposito;
+
+//-----------------------------------------------	
+	public Atributos(int numero_conta) {
+		this.numero_conta = numero_conta;
+	}
+
+//-----------------------------------------------	
+	public Atributos(String nome, double deposito) {
+		this.nome = nome;
+		this.deposito = deposito;
+	}
+
+//-----------------------------------------------
+
+	public int getNumero_conta() {
+		return numero_conta;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getDeposito() {
+		return deposito;
+	}
+
+	public void setDeposito(double deposito) {
+		this.deposito = deposito;
+	}
+
+//-----------------------------------------------
+	public void Deposito(double deposito) {
+		setDeposito(deposito);
+		
+	}
 }
--- Exercicio05/src/Exe05/Main.java
+++ Exercicio05/src/Exe05/Main.java
@@ -1,10 +1,52 @@
 package Exe05;
 
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Informações bancárias ");
+		int numero_conta = ThreadLocalRandom.current().nextInt(100000, 1000000);
+		Atributos atributos = new Atributos(numero_conta);
+		System.out.println("NUMERO DA SUA CONTA É: " + atributos.getNumero_conta());
+		
+		
+		System.out.print("Informe o nome do titular da conta: ");
+		String nome = sc.nextLine();
+		
+		
+		System.out.print("Gostaria de DEPOSITAR algum Valor: [ Sim ou Não ] : ");
+	    String entrada = sc.nextLine().toLowerCase();
+	    double deposito = 0;
+	    if (entrada.equals("sim")) {
+			System.out.print("Qual o valor para o deposito");
+			deposito = sc.nextDouble();
+	    } else {
+			System.out.println("Você escolheu 'não'.");
+		}
+		Atributos dados = new Atributos(nome, deposito);
+		dados.setDeposito(deposito);
+//--------------------------------------		
+		System.out.println("CONTA: " 
+				+ atributos.getNumero_conta() + ", Titular: " 
+				+ dados.getNome() + ", " 
+				+ "Saldo em conta: " 
+				+ dados.getDeposito() 
+				+ " R$");
+		
+//--------------------------------------
+		
+		
+		
+		System.out.println("OBRIGADO");
+		sc.close();
 	}
 
 }
