5916167fdb742ab993aeb7a721f21ba1f09cb84e
ivan Vitti <ivan.vitti0@gmail.com>
2023-10-30 10:38:58 -0300

    novo01

--- Exercicios06/src/Exer06/Principal.java
+++ Exercicios06/src/Exer06/Principal.java
@@ -10,10 +10,40 @@ public class Principal {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Informe um numero: ");
-		int n = sc.nextInt();
+	
 		
-		double[] Vetor = new double[n];
+		Atributos[] Vetor = new Atributos[2];
+		double soma = 0.0;
+		
+		for ( int i = 0; i < Vetor.length; i++) {
+			System.out.print("Informe o nome do produto: ");
+			String name = sc.nextLine();
+			System.out.print("Informe o preço do produto: ");
+			double preco = sc.nextDouble();
+			System.out.print("Informe a Quantidade do produto: ");
+			int quant = sc.nextInt();
+			
+			Vetor[i] = new Atributos(name, preco, quant);
+					
+			soma += (Vetor[i].getPreco() * Vetor[i].getQuant() );
+			
+			sc.nextLine();
+		}
+		
+		System.out.print("o valor total e Reais que temos no estoque é:  " + soma);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+/*		int n = sc.nextInt();
+ 		double[] Vetor = new double[n];
 		double m = 0.0;
 		
 		for ( int i = 0; i < n; i++) {
@@ -22,7 +52,7 @@ public class Principal {
 			m += Vetor[i];
 		}
 		System.out.print("O total dos valores é: " + m + " A media dos valores é: " + m/n );
-		
+*/		
 		
 		
 		
